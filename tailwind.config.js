68754dbfd93454066d3dd582cd378a540f94350f
CTZDev <carlosteranzavaleta@hotmail.com>
2024-08-13 17:19:36 -0500

    feat: Add custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clr-primary': '#03045E',
+        'clr-secondary': '#023E8A',
+        'clr-success': '#00cc99',
+        'clr-error': '#FF758F',
+        'clr-info': '#0077B6',
+        'bg-primary': '#48CAE4',
+        'bg-secondary': '#90E0EF',
+      },
+    },
   },
   plugins: [],
 };
