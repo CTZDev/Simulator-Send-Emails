e161c451cab390abb8c91470b151bdbcb573dacd
CTZDev <carlosteranzavaleta@hotmail.com>
2024-08-14 20:20:38 -0500

    Changed name component Inputs for InputGroup

--- src/SimulatorEmailApp.tsx
+++ src/SimulatorEmailApp.tsx
@@ -1,7 +1,7 @@
 import type React from 'react';
-import Buttons from './Buttons';
-import FormEmail from './FormEmail';
-import Inputs from './Inputs';
+import Buttons from './components/Buttons';
+import FormEmail from './components/FormEmail';
+import InputGroup from './components/InputGroup';
 import '@fontsource/poppins/300.css';
 import '@fontsource/poppins/400.css';
 import '@fontsource/poppins/600.css';
@@ -9,7 +9,7 @@ import '@fontsource/poppins/600.css';
 const SimulatorEmailApp: React.FC = () => {
   return (
     <FormEmail>
-      <Inputs />
+      <InputGroup />
       <Buttons />
     </FormEmail>
   );
