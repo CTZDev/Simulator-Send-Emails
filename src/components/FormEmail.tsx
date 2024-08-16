204427c54e8f254e9a436115163e01d1c9710502
CTZDev <carlosteranzavaleta@hotmail.com>
2024-08-15 21:55:11 -0500

    Add form

--- src/components/FormEmail.tsx
+++ src/components/FormEmail.tsx
@@ -8,7 +8,9 @@ const FormEmail: React.FC<Props> = ({ children }) => {
       <h2 className='text-center text-balance text-3xl p-8 font-semibold text-clr-primary md:text-5xl md:p-16'>
         Enviar emails 📧
       </h2>
-      {children}
+      <form className='max-w-[768px] m-auto px-16 py-14 text-sm rounded-xl shadow-2xl shadow-black bg-white md:text-base'>
+        {children}
+      </form>
     </>
   );
 };
