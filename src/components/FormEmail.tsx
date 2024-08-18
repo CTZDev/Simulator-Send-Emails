3c3ea76af594d0c62f19e37467a8b921e6bf79df
CTZDev <carlosteranzavaleta@hotmail.com>
2024-08-18 18:07:48 -0500

    Add form Email

--- src/components/FormEmail.tsx
+++ src/components/FormEmail.tsx
@@ -1,14 +1,41 @@
+import { useRef } from 'react';
+import Swal from 'sweetalert2';
+
 interface Props {
   children: React.ReactNode;
 }
 
 const FormEmail: React.FC<Props> = ({ children }) => {
+  const formRef = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    Swal.fire({
+      position: 'center',
+      icon: 'success',
+      title: 'Your e-mail has been sent successfully',
+      showConfirmButton: false,
+      timer: 1500,
+    }).then(() => {
+      if (formRef.current) {
+        formRef.current.reset(); // Limpiar los campos del formulario
+        //formRef.current.submit(); // Enviar el formulario
+      }
+    });
+  };
+
   return (
     <>
       <h2 className='text-center text-balance text-3xl p-8 font-semibold text-clr-primary md:text-5xl md:p-16'>
         Enviar emails 📧
       </h2>
-      <form className='max-w-[768px] m-auto px-16 py-14 text-sm rounded-xl shadow-2xl shadow-black bg-white md:text-base'>
+      <form
+        action='/'
+        method='POST'
+        ref={formRef}
+        onSubmit={sendEmail}
+        className='max-w-[768px] m-auto px-16 py-14 text-sm rounded-xl shadow-2xl shadow-black bg-white md:text-base'>
         {children}
       </form>
     </>
