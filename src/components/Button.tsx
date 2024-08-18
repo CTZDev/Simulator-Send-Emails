fbcfdc900358cd1c54b0acfe2c84a3343c1259bf
CTZDev <carlosteranzavaleta@hotmail.com>
2024-08-18 18:07:04 -0500

    Add component Button and props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,23 @@
 import { ButttonProps as Props } from '../types/Button';
+import Image from './Image';
 
-const Button: React.FC<Props> = ({ id, type, content, className }) => {
+const Button: React.FC<Props> = ({
+  id,
+  type,
+  content,
+  className,
+  imageIcon,
+}) => {
   return (
     <button
       type={type}
       id={id}
-      className={`${className} p-2.5 w-44 border rounded-lg text-base font-semibold md:text-lg`}>
-      {content}
+      className={`${className} p-2.5 w-44 border rounded-lg text-base font-semibold flex items-center justify-center gap-2.5 transition hover:scale-[1.025] md:text-lg`}
+      value={type.charAt(0).toUpperCase().concat(type.slice(1))}>
+      {imageIcon && (
+        <Image src={imageIcon.src} alt={imageIcon.alt} width={28} height={28} />
+      )}
+      <p>{content}</p>
     </button>
   );
 };
