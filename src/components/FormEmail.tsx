3bb9f4532165b84ccb8051ff3291620048a0b437
CTZDev <carlosteranzavaleta@hotmail.com>
2024-08-19 18:23:46 -0500

    refactor: Add funcionality of form

--- src/components/FormEmail.tsx
+++ src/components/FormEmail.tsx
@@ -1,4 +1,11 @@
-import { useRef } from 'react';
+import {
+  Children,
+  cloneElement,
+  isValidElement,
+  type ReactElement,
+} from 'react';
+import { useEffect, useRef, useState } from 'react';
+import ButtonGroup from './ButtonGroup';
 import Swal from 'sweetalert2';
 
 interface Props {
@@ -6,24 +13,41 @@ interface Props {
 }
 
 const FormEmail: React.FC<Props> = ({ children }) => {
+  const [isFormValid, setIsFormValid] = useState(false);
   const formRef = useRef<HTMLFormElement>(null);
+
+  const checkFormValidity = () => {
+    if (formRef.current) {
+      const form = formRef.current;
+      const isValid = form.checkValidity();
+      setIsFormValid(isValid);
+    }
+  };
 
   const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    Swal.fire({
-      position: 'center',
-      icon: 'success',
-      title: 'Your e-mail has been sent successfully',
-      showConfirmButton: false,
-      timer: 1500,
-    }).then(() => {
-      if (formRef.current) {
-        formRef.current.reset(); // Limpiar los campos del formulario
-        //formRef.current.submit(); // Enviar el formulario
-      }
-    });
+    if (isFormValid) {
+      Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Your e-mail has been sent successfully',
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        if (formRef.current) {
+          formRef.current.reset(); // Limpiar los campos del formulario
+          setIsFormValid(false); // Deshabilitar el botón nuevamente
+          // Forzar una actualización de la validez después del reinicio
+          checkFormValidity(); // Revalidar el formulario después de reiniciar
+        }
+      });
+    }
   };
+
+  useEffect(() => {
+    checkFormValidity(); // Verificar validez al cargar el componente
+  }, []);
 
   return (
     <>
@@ -31,12 +55,21 @@ const FormEmail: React.FC<Props> = ({ children }) => {
         Enviar emails 📧
       </h2>
       <form
-        action='/'
+        action='index.html'
         method='POST'
         ref={formRef}
         onSubmit={sendEmail}
+        onChange={checkFormValidity} // Verificar validez en tiempo real
         className='max-w-[768px] m-auto px-16 py-14 text-sm rounded-xl shadow-2xl shadow-black bg-white md:text-base'>
-        {children}
+        {Children.map(children, (child) => {
+          if (isValidElement(child) && child.type === ButtonGroup) {
+            return cloneElement(
+              child as ReactElement<{ isFormValid: boolean }>,
+              { isFormValid }
+            );
+          }
+          return child;
+        })}
       </form>
     </>
   );
