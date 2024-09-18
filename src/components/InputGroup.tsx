bd3ea8aec4e64c71e9ee43986d01a86fc8397326
CTZDev <carlosteranzavaleta@hotmail.com>
2024-09-17 20:20:53 -0500

    refactor: Optimize Input and TextArea props

--- src/components/InputGroup.tsx
+++ src/components/InputGroup.tsx
@@ -7,30 +7,13 @@ const InputGroup: React.FC = () => {
   return (
     <div className='flex flex-col gap-6 mb-6'>
       {/* Section of Inputs */}
-      {ATTRIBUTES_INPUTS.map(
-        ({ label, id, type, title, placeholder, pattern }) => (
-          <Input
-            key={id}
-            label={label}
-            id={id}
-            type={type}
-            title={title}
-            placeholder={placeholder}
-            pattern={pattern}
-          />
-        )
-      )}
+      {ATTRIBUTES_INPUTS.map((data) => (
+        <Input key={data.id} {...data} />
+      ))}
 
       {/* Section of TextArea */}
-      {ATTRIBUTES_TEXTAREA.map(({ label, id, title, pattern, placeholder }) => (
-        <TextArea
-          key={id}
-          label={label}
-          id={id}
-          title={title}
-          placeholder={placeholder}
-          pattern={pattern}
-        />
+      {ATTRIBUTES_TEXTAREA.map((data) => (
+        <TextArea key={data.id} {...data} />
       ))}
     </div>
   );
